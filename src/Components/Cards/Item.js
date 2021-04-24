fc5b9a812e37bea7c28d4d2b6aa1ce37767ceeb3
Harshit Shukla <Harshitshukla151001@gmail.com>
2021-04-25 00:12:28 +0530

    color segregation added

--- src/Components/Cards/Item.js
+++ src/Components/Cards/Item.js
@@ -8,10 +8,14 @@ const Item = ({
   contactNumber,
   stock,
   address,
+  user,
 }) => {
   return (
     <div style={{ zIndex: "0" }} className="col-md col-md-6">
-      <div className="card shadow rounded input-group p-2 px-3 my-2 card-special">
+      <div
+        style={{ borderColor: user ? "#198754" : "#0099ff" }}
+        className="card shadow rounded input-group p-2 px-3 my-2 card-special"
+      >
         <div className="mb-0 d-flex justify-content-between align-items-center">
           <div className="h4 my-0 text-success">{name}</div>
           <div className="my-0">
