9137a1d2d117190a7b0301bc8e96d688df531ce8
Harshit Shukla <Harshitshukla151001@gmail.com>
2021-04-26 08:56:01 +0530

    push

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./root.scss";
+import ReactGA from "react-ga";
+ReactGA.initialize("UA-167636257-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
