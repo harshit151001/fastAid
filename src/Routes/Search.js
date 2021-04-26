0165ef98705763d5385bf86a618407bc4b26edba
Harshit Shukla <Harshitshukla151001@gmail.com>
2021-04-26 21:18:25 +0530

    push

--- src/Routes/Search.js
+++ src/Routes/Search.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import Item from '../Components/Cards/Item';
-import ItemsList from '../Components/Lists/ItemsList';
-import { withRouter, Link } from 'react-router-dom';
-import { getItemsFromQuery } from '../Helper/Enpoints/Endpoints';
-import Select from 'react-select';
+import React, { useState, useEffect } from "react";
+import Item from "../Components/Cards/Item";
+import ItemsList from "../Components/Lists/ItemsList";
+import { withRouter, Link } from "react-router-dom";
+import { getItemsFromQuery } from "../Helper/Enpoints/Endpoints";
+import Select from "react-select";
 
-const Search = props => {
+const Search = (props) => {
   const { cityId, searchQuery, page } = props.match.params;
   const [items, setItems] = useState([]);
-  const filterOptions = [{ _id: 'likes', name: 'Most Liked' }];
+  const filterOptions = [{ _id: "likes", name: "Most Liked" }];
 
-  const [selectedFilter, setSelectedFilter] = useState('');
+  const [selectedFilter, setSelectedFilter] = useState("");
 
-  const onchangeSelect = filter => {
+  const onchangeSelect = (filter) => {
     setSelectedFilter(filter);
   };
 
@@ -20,7 +20,12 @@ const Search = props => {
     let mounted = true;
     if (mounted) {
       const getAndSetItems = async () => {
-        const response = await getItemsFromQuery(page, cityId, searchQuery, selectedFilter._id);
+        const response = await getItemsFromQuery(
+          page,
+          cityId,
+          searchQuery,
+          selectedFilter._id
+        );
         console.log(response);
         setItems(response);
       };
@@ -30,30 +35,94 @@ const Search = props => {
   }, [cityId, page, searchQuery, selectedFilter]);
 
   return (
-    <div style={{ background: '#fafafa' }}>
-      <div className="alert alert-success mt-0 mb-1 justify-content-between align-items-center" role="alert">
-        <span className="lead order-1">Know some resources? </span>
-        <Link to={{ pathname: '/login', state: { seller: 0 } }}>
-          <button className="btn btn-success">Add Info</button>
-        </Link>
-      </div>
-      <div>
-        Sort By: <Select value={selectedFilter} onChange={onchangeSelect} options={filterOptions} getOptionValue={option => option._id} getOptionLabel={option => option.name} />
+    <div style={{ background: "#fafafa" }}>
+      <div
+        style={{ paddingTop: "8px", paddingBottom: "8px" }}
+        className="alert d-md-flex alert-success mt-0 mb-1 justify-content-between align-items-center"
+        role="alert"
+      >
+        <div className="d-flex align-items-center justify-content-between">
+          <div className="lead order-1 pe-3">Know some resources? </div>
+          <Link
+            className="order-2"
+            to={{ pathname: "/login", state: { seller: 0 } }}
+          >
+            <button className="btn btn-success">Add Info</button>
+          </Link>
+        </div>
+        <div className="pt-2 d-flex align-items-center">
+          <div className="text-success lead pe-3">Sort By: </div>
+          <div style={{ minWidth: "200px" }} className="col">
+            <Select
+              value={selectedFilter}
+              onChange={onchangeSelect}
+              options={filterOptions}
+              getOptionValue={(option) => option._id}
+              getOptionLabel={(option) => option.name}
+            />
+          </div>
+        </div>
       </div>
       <ItemsList>
         {items.length > 0 ? (
-          items.map(({ name, stock, _id, contactNumber, address, companyName, city, likes, dislikes, createdAt, updatedAt }) => <Item name={name} stock={stock} key={_id} id={_id} likes={likes} dislikes={dislikes} city={city} companyName={companyName} address={address} contactNumber={contactNumber} createdAt={createdAt} updatedAt={updatedAt} />)
+          items.map(
+            ({
+              name,
+              stock,
+              _id,
+              contactNumber,
+              address,
+              companyName,
+              city,
+              likes,
+              dislikes,
+              createdAt,
+              updatedAt,
+            }) => (
+              <Item
+                name={name}
+                stock={stock}
+                key={_id}
+                id={_id}
+                likes={likes}
+                dislikes={dislikes}
+                city={city}
+                companyName={companyName}
+                address={address}
+                contactNumber={contactNumber}
+                createdAt={createdAt}
+                updatedAt={updatedAt}
+              />
+            )
+          )
         ) : (
           <div className="p-4">
-            <p class="display-4 text-center">Sorry, No items matched your search.</p>
+            <p class="display-4 text-center">
+              Sorry, No items matched your search.
+            </p>
           </div>
         )}
       </ItemsList>
       <div className="d-flex align-items-center justify-content-center p-4">
-        <button disabled={page === 1} onClick={() => props.history.push(`/search/${cityId}/${searchQuery}/${+page > 1 ? +page - 1 : +page}`)} className="btn btn-success mx-2">
+        <button
+          disabled={page === 1}
+          onClick={() =>
+            props.history.push(
+              `/search/${cityId}/${searchQuery}/${
+                +page > 1 ? +page - 1 : +page
+              }`
+            )
+          }
+          className="btn btn-success mx-2"
+        >
           Back
         </button>
-        <button onClick={() => props.history.push(`/search/${cityId}/${searchQuery}/${+page + 1}`)} className="btn btn-success mx-2">
+        <button
+          onClick={() =>
+            props.history.push(`/search/${cityId}/${searchQuery}/${+page + 1}`)
+          }
+          className="btn btn-success mx-2"
+        >
           Next
         </button>
       </div>
